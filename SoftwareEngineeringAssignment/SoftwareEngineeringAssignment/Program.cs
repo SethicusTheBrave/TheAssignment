69db642586dab8ce0cfc1a3d52925ce0b193fcf2
Matas <matas9745@gmail.com>
2017-11-04 21:28:40 +0000

    merge

--- SoftwareEngineeringAssignment/SoftwareEngineeringAssignment/Program.cs
+++ SoftwareEngineeringAssignment/SoftwareEngineeringAssignment/Program.cs
@@ -16,8 +16,9 @@ namespace SoftwareEngineeringAssignment
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            // Application.Run(new Login());
-              Application.Run(new RegisterPatient());
+            Application.Run(new Login());
+            //  Application.Run(new RegisterPatient());
+
         }
     }
 }
