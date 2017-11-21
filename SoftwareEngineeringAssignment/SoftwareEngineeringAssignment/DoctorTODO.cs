56e4a8ed6df340cc280cece0c4a5ee4ace1ea795
Seth Tilley <370990@edu.cwa.ac.uk>
2017-11-21 09:24:45 +0000

    Merging

--- SoftwareEngineeringAssignment/SoftwareEngineeringAssignment/DoctorTODO.cs
+++ SoftwareEngineeringAssignment/SoftwareEngineeringAssignment/DoctorTODO.cs
@@ -12,13 +12,20 @@ namespace SoftwareEngineeringAssignment
 {
     public partial class DoctorTODO : Form
     {
+        List<Patient> patientList;
+        BusinessMetaLayer instance;
         public DoctorTODO()
         {
             InitializeComponent();
         }
-
+        /// <summary>
+        /// When The individual double clicks on a value in the table will open the patient details
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void dataGridView1_CellContentDoubleClick(object sender, DataGridViewCellEventArgs e)
         {
+            patientList = instance.patientList();
 
         }
     }
